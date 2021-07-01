57e1dff1f0875796180f8770f5233264564ad7d1
gun3486 <73639169+gun3486@users.noreply.github.com>
2021-07-01 15:59:54 +0900

    스탑워치
    스탑위치만들기 아직 버그가 많음

--- StopWatch/StopWatch/ViewController.swift
+++ StopWatch/StopWatch/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var time: UILabel!
+    @IBOutlet var txtTime: UILabel!
+    
+    var count: Int = 0
+    var mTimer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func btnStart(_ sender: UIButton) {
+        if mTimer != nil{
+            mTimer = Timer.scheduledTimer(timeInterval: 0.035, target: self, selector: #selector(timerCallback), userInfo: nil, repeats: true)
+        } else {
+            mTimer = Timer.scheduledTimer(timeInterval: 0.035, target: self, selector: #selector(timerCallback), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func btnStop(_ sender: UIButton) {
+        if mTimer != nil {
+            mTimer.invalidate()
+        }
+        count = 0
+        txtTime.text = String(count)
+    }
+    
+    @objc func timerCallback() {
+        count += 1
+        txtTime.text = String(count)
+    }
 }
+
 
