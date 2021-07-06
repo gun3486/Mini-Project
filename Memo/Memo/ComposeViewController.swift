17c56da55e0be7ee50e55bc7f5ca3146d1cc2fe4
gun3486 <73639169+gun3486@users.noreply.github.com>
2021-07-06 17:04:25 +0900

    cancel 버튼

--- Memo/Memo/ComposeViewController.swift
+++ Memo/Memo/ComposeViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    @IBOutlet var close: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func close(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
     }
-    */
-
+    
 }
