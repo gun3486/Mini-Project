ed7a48ae877c9acc0c0030a05d3b5cd7a4224d0b
gun3486 <73639169+gun3486@users.noreply.github.com>
2021-07-07 10:21:31 +0900

    Memo class

---
+++ MemoApp/MemoApp/Model/Model.swift
@@ -0,0 +1,22 @@
+
+
+import Foundation
+
+
+class Memo {
+    
+    var content: String
+    var insertDate: Date
+        
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyDataList = [
+        
+        Memo(content: "Yuri = 🐹"),
+        Memo(content: "Yena = 🐥")
+    ]
+    
+}
