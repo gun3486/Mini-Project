d4e9af845df748325b132cd786891069f915232b
gun3486 <73639169+gun3486@users.noreply.github.com>
2021-07-07 11:06:28 +0900

    새 메모 확인

--- MemoApp/MemoApp/MemoListTableViewController.swift
+++ MemoApp/MemoApp/MemoListTableViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class MemoListTableViewController: UITableViewController {
 
+    let formatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        return f
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,8 +41,7 @@ class MemoListTableViewController: UITableViewController {
 
         let target = Memo.dummyDataList[indexPath.row]
         cell.textLabel?.text = target.content
-        cell.detailTextLabel?.text = target.insertDate.description
-
+        cell.detailTextLabel?.text = formatter.string(from: target.insertDate)
         return cell
     }
     
