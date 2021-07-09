4a8152d0070699d37eafe8442d934368fd378732
gun3486 <73639169+gun3486@users.noreply.github.com>
2021-07-09 16:01:40 +0900

    이미지 랜덤표시
    이미지 랜덤표시앱 간단하게 만듬

--- RandomPhoto/RandomPhoto/ViewController.swift
+++ RandomPhoto/RandomPhoto/ViewController.swift
@@ -15,6 +15,23 @@ class ViewController: UIViewController {
         imageView.backgroundColor = .white
         return imageView
     }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Radom Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    let colors: [UIColor] = [
+        .systemRed,
+        .systemBlue,
+        .systemPink,
+        .systemIndigo,
+        .systemYellow,
+        .systemPurple
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,11 +39,31 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
+        
+        view.addSubview(button)
+        getRandomPhoto()
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    @objc func didTapButton() {
+        getRandomPhoto()
+        
+        view.backgroundColor = colors.randomElement()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x:  20, y: view.frame.size.height-150-view.safeAreaInsets.bottom, width: view.frame.size.width-60, height: 50)
+        
     }
 
     func getRandomPhoto() {
         let urlString = "https://source.unsplash.com/random/600x600"
-        let url = URL(string: urlString)
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
     }
     
 }
