3fc74318a9268f11bf55fe3833ed63f18d59979b
gun3486 <73639169+gun3486@users.noreply.github.com>
2021-08-12 16:03:07 +0900

    버튼/오류
    버튼/오류

--- MovieProject/MovieProject/View Controllers/SignUpViewController.swift
+++ MovieProject/MovieProject/View Controllers/SignUpViewController.swift
@@ -13,6 +13,8 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
